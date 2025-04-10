c36972e640a846963675b9e9098fcc363ddc0c03
Milena Famler <m.famler.students.htl-leonding.ac.at>
2025-04-10 14:46:11 +0200

    work on acc setup

---
+++ project/accSetUp.js
@@ -0,0 +1,12 @@
+function save() {
+    console.log("save");
+}
+
+function changePfp() {
+    console.log("pfp");
+}
+
+function checkIfPwAlign() {
+    let pw1 = document.getElementById("pw1").value;
+    let pw1 = document.getElementById("pw1").value;
+}
