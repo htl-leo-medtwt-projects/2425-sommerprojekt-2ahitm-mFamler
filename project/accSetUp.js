49888836e8cf1cc5f4abea1d700ec54af2ca6559
Milena Famler <m.famler.students.htl-leonding.ac.at>
2025-04-10 15:27:07 +0200

    work on acc setup

--- project/accSetUp.js
+++ project/accSetUp.js
@@ -1,5 +1,7 @@
 function save() {
     console.log("save");
+    checkIfPwAlign();
+    checkIfUserLongEnough();
 }
 
 function changePfp() {
@@ -8,5 +10,29 @@ function changePfp() {
 
 function checkIfPwAlign() {
     let pw1 = document.getElementById("pw1").value;
-    let pw1 = document.getElementById("pw1").value;
+    let pw2 = document.getElementById("pw2").value;
+
+    if(pw1 != false) {
+        if(pw1 == pw2) {
+            console.log("same");
+        }
+        else {
+            console.log("not same");
+        }
+    }
+    else {
+        console.log("pw1 is missing")
+    }
+}
+
+function checkIfUserLongEnough() {
+    let name = document.getElementById("name").value;
+    console.log("name:" + name);
+
+    if(name.length == false) {
+        console.log("user name too short");
+    }
+    else {
+        console.log("long enough");
+    }
 }
