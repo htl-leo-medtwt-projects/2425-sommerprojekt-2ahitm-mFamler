7ed9afac467b0c1218591528acad37099de9d3ff
Milena Famler <m.famler.students.htl-leonding.ac.at>
2025-04-11 14:27:11 +0200

    work on homepage and js

--- project/homepage.js
+++ project/homepage.js
@@ -2,6 +2,9 @@
 let cardCont;
 let imgs;
 let vid;
+let bmFilled = "";
+let isFilled = false;
+
 
 //func
 function fetchAnimal() {
@@ -9,11 +12,23 @@ function fetchAnimal() {
     .then( (response) => {
         return response.json();
     })
-    .then( (animal))
+    .then( (animal) => {
+        console.log(animal);
+    })
+    .catch( (error) => {
+        throw error;
+    })
 }
 
-function bookmark() {
-
+function bookmark(nr) {
+    if(isFilled == false) {
+        document.getElementById(`bm${nr}`).src = "images/icons/bookmarkfilledbluewithshadownew.png";
+        isFilled = true;
+    }
+    else {
+        document.getElementById(`bm${nr}`).src = "images/icons/bookmarkOGwithshadownew.png";
+        isFilled = false;
+    }
 }
 
 function heart() {
@@ -25,26 +40,26 @@ function accSetUpQuick() {
 }
 
 function loadCards() {
-    cardCont += `
-        <div class="card">
+    /*cardCont += `
+       <div class="card">
             <div class="try_card">
                 <img src="images/icons/bookmarkOGwithshadownew.png" alt="bookmark" class="bookmark" onclick="bookmark()">
 
-                <div class="card-img" style="background-image: url(${img});" id="card1"></div>  
+                <div class="card-img" style="background-image: url(images/seal-lion.jpg);" id="card1"></div>  
 
                 <div class="card_txt">
-                    <h1 class="name, txt" >${name}</h1>
-                    <h3 class="txt" >${latin name}</h3>
-                    <p class="txt" >${location}</p>
-                    <p class="txt">${family}</p>
-                    <a href="${vid}" class="txt" >video</a><br>
-                    <a href="${artc}" class="txt" >article</a><br>
+                    <h1 class="name, txt" >Sea Lion</h1>
+                    <h3 class="txt" >Otariinae</h3>
+                    <p class="txt" >Pacific ocean</p>
+                    <p class="txt">family</p>
+                    <a href="#" class="txt" >video</a><br>
+                    <a href="#" class="txt" >article</a><br>
                 </div>
 
                 <img src="images/icons/heart.png" alt="heart" class="heart" onclick="heart()">
             </div>
         </div>
-    `
+    `*/
 }
 
 function loadImgs() {
@@ -56,4 +71,4 @@ function loadVid() {
 }
 
 //calling
-
+fetchAnimal();
